854e84c010310e3824dda5212a3cd6755cd0f8db
Evelyn Lee <evelynlee8@gmail.com>
2021-07-13 02:22:24 +0800

    練習

--- assets/js/tw101.js
+++ assets/js/tw101.js
@@ -9,7 +9,7 @@ function playAudio(vid) {
 	}
 }
 
-function playAnswer(vid) {
+function playAnswer1(vid) {
 	$('audio').each(function(){
 		this.pause();
 	});
@@ -18,5 +18,55 @@ function playAnswer(vid) {
 		x.currentTime = 0;
 		x.play();
 	}
-	$('#rAnswer').show();
+	$('#rAnswer1').show();
+}
+
+function playAnswer2(vid) {
+	$('audio').each(function(){
+		this.pause();
+	});
+	x = document.getElementById("m_"+vid);
+	if(x){
+		x.currentTime = 0;
+		x.play();
+	}
+	$('#rAnswer2').show();
+	$('#rAnswer3').show();
+}
+
+function playAnswer3(vid) {
+	$('audio').each(function(){
+		this.pause();
+	});
+	x = document.getElementById("m_"+vid);
+	if(x){
+		x.currentTime = 0;
+		x.play();
+	}
+	$('#rAnswer4').show();
+	$('#rAnswer5').show();
+}
+
+function playAnswer4(vid) {
+	$('audio').each(function(){
+		this.pause();
+	});
+	x = document.getElementById("m_"+vid);
+	if(x){
+		x.currentTime = 0;
+		x.play();
+	}
+	$('#rAnswer6').show();
+}
+
+function playAnswer5(vid) {
+	$('audio').each(function(){
+		this.pause();
+	});
+	x = document.getElementById("m_"+vid);
+	if(x){
+		x.currentTime = 0;
+		x.play();
+	}
+	$('#rAnswer7').show();
 }
